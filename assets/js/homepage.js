a246f0e1397801a80a92fd833732dbb22429e414
KerriAnn1 <dragonsfire20@hotmail.com>
2022-02-24 11:32:56 -0500

    adding columns

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,43 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repo";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            displayRepos(data, user);
+          console.log(data);
+        });
+      });
+
+      var formSubmitHandler = function(event) {
+        event.preventDefault();
+
+     // get value from input element
+      var username = nameInputEl.value.trim();
+
+        if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+        } else {
+        alert("Please enter a GitHub username");
+}
+   
+        console.log(event);
+
+        var displayRepos = function(repos, searchTerm) {
+            console.log(repos);
+            console.log(searchTerm);
+          };
+      };
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler)
   
-  getUserRepos();
+  
